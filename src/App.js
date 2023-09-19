f5e4e113f28ad6035848d17fc92ab2dd53963c63
MiUlon <mindegi@gmail.com>
2023-09-19 12:08:30 +0300

    adding SignInForm ccomponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './routes/home/home.components';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 import { Routes, Route } from 'react-router-dom';
 
 const Test = () => {
@@ -17,7 +17,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path='test' element={<Test />} />
-        <Route path='sign-in' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
