73218f221727f381b268d2a56d5062c926b7ceeb
MiUlon <mindegi@gmail.com>
2023-10-23 11:32:16 +0300

    implementing Checkout component

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import './checkout.styles.scss';
+
+const Checkout = () => {
+    const { cartItems, addItemToCart } = useContext(CartContext);
+
+    return(
+        <div>
+            <h1>This is Checkout page!</h1>
+            <div>
+                {cartItems.map((cartItem) => {
+                    const { id, name, quantity } = cartItem;
+                    return(
+                        <div key={id}>
+                            <h2>{name}</h2>
+                            <span>{quantity}</span>
+                            <br />
+                            <span>Decrease</span>
+                            <br />
+                            <span onClick={() => addItemToCart(cartItem)}>Increase</span>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
