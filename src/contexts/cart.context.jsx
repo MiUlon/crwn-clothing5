7dc43096384cd3c8d8850a012264a8af8312d30b
MiUlon <mindegi@gmail.com>
2023-11-09 06:39:29 +0200

    implemented functionality to calculate total cart price

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -37,16 +37,24 @@ export const CartContext = createContext({
     removeItemFromCart: () => { },
     clearItemFromCart: () => { },
     cartCount: 0,
+    cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
 
     useEffect(() => {
         const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
         setCartCount(newCartCount);
+    }, [cartItems]);
+
+    useEffect(() => {
+        const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price, 0);
+
+        setCartTotal(newCartTotal);
     }, [cartItems]);
 
     const addItemToCart = (productToAdd) => {
@@ -61,7 +69,7 @@ export const CartProvider = ({ children }) => {
         setCartItems(clearCartItem(cartItems, cartItemToClear));
     };
 
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart };
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount, removeItemFromCart, clearItemFromCart, cartTotal };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
