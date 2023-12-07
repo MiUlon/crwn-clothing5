c3c297879ff6f391fe1d3fe5c706545702ddf608
MiUlon <mindegi@gmail.com>
2023-12-07 12:33:35 +0200

    adding some stylings

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router-dom';
-import { useContext, useEffect, useState } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../product-card/product-card.component';
 import './category.styles.scss';
@@ -14,9 +14,12 @@ const Category = () => {
     }, [category, categoriesMap]);
 
     return (
-        <div className='category-container'>
-            {products && products.map((product) => <ProductCard key={product.id} product={product}/>)}
-        </div>
+        <Fragment>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className='category-container'>
+                {products && products.map((product) => <ProductCard key={product.id} product={product} />)}
+            </div>
+        </Fragment>
     );
 };
 
